c47d49d1f28e1e68cecab20cd65c37594dcfd52c
kanan-logiciel <kanan@logiciel.io>
2026-03-24 03:42:05 +0530

    fix: auth-page fixed

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
+import Login from "./auth/login/page";
+
 export default function Home() {
-  return (
-    <div className="flex items-center justify-center h-screen">
-      <h1 className="text-2xl font-bold text-blue-500">Work-Nest Tracker 🚀</h1>
-    </div>
-  );
+  return <Login />;
 }
