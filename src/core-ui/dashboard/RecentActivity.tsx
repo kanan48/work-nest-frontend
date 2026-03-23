c85f17e644f397e6c822660370f31488dcf91252
kanan-logiciel <kanan@logiciel.io>
2026-03-23 05:40:11 +0530

    fix: updated description

--- src/core-ui/dashboard/RecentActivity.tsx
+++ src/core-ui/dashboard/RecentActivity.tsx
@@ -6,9 +6,9 @@ export default function RecentActivity() {
       </h2>
 
       <ul className="text-sm space-y-2 text-gray-600 dark:text-gray-300">
-        <li>Rahul moved task &quot;UI Fix&quot; to Done</li>
-        <li>Anjali created new task &quot;Login API&quot;</li>
-        <li>You updated project &quot;WorkNest&quot;</li>
+        <li>1.Rahul moved task &quot;UI Fix&quot; to Done</li>
+        <li>2.Anjali created new task &quot;Login API&quot;</li>
+        <li>3.You updated project &quot;WorkNest&quot;</li>
       </ul>
     </div>
   );
